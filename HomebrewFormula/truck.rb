a08d76d5b0abd7b196e540dde2fe67077012f5e5
Maz Jaleel <mazjaleel@gmail.com>
2017-11-18 00:10:48 +0300

    version bump

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -1,7 +1,7 @@
 class Truck < Formula
   desc "Truck - the simplest dependency manager"
   url "https://github.com/Mazyod/homebrew-truck/archive/master.zip"
-  version "0.0.5"
+  version "0.1.0"
   # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
