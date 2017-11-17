20647566c411ef382e8625a779f274afef215095
Maz Jaleel <mazjaleel@gmail.com>
2017-11-17 08:25:56 +0400

    this should do it

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -1,11 +1,11 @@
 class Truck < Formula
   desc "Truck - the simplest dependency manager"
-  url "https://github.com/mazyod/homebrew-truck"
+  url "https://github.com/Mazyod/homebrew-truck/archive/master.zip"
   version "0.0.1"
   # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
-    bin.install "../truck-client.py" => "truck"
+    bin.install "truck-client.py" => "truck"
   end
 
   test do
