844329a2fc88cde1dffb1ac9ae5eef9963c98033
Maz Jaleel <mazjaleel@gmail.com>
2017-11-17 08:12:37 +0400

    more attempts

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -4,8 +4,7 @@ class Truck < Formula
   version "0.0.1"
 
   def install
-    libexec.install "truck-client.py"
-    bin.install_symlink libexec/"truck-client.py" => "truck"
+    bin.install "truck-client.py" => truck
   end
 
   test do
