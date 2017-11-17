cfe550a3c03023d670b5ea359dc5c04141da4885
Maz Jaleel <mazjaleel@gmail.com>
2017-11-17 08:22:25 +0400

    strill trying

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -1,10 +1,11 @@
 class Truck < Formula
-  desc "Truck."
+  desc "Truck - the simplest dependency manager"
   url "https://github.com/mazyod/homebrew-truck"
   version "0.0.1"
+  # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
-    bin.install "truck-client.py" => "truck"
+    bin.install "../truck-client.py" => "truck"
   end
 
   test do
