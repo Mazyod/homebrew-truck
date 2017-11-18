a0d417a3d3c9408fb3896105b17f02cd76c78141
Maz Jaleel <mazjaleel@gmail.com>
2017-11-18 08:40:51 +0300

    new release

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -1,7 +1,7 @@
 class Truck < Formula
   desc "Truck - the simplest dependency manager"
-  url "https://github.com/Mazyod/homebrew-truck/archive/0.1.2.zip"
-  version "0.1.2"
+  url "https://github.com/Mazyod/homebrew-truck/archive/0.1.3.zip"
+  version "0.1.3"
   # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
