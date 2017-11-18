074626d74ecbf793964d93f14d626c0b751af1c6
Maz Jaleel <mazjaleel@gmail.com>
2017-11-18 13:48:02 +0300

    show file url when done

--- HomebrewFormula/truck.rb
+++ HomebrewFormula/truck.rb
@@ -1,7 +1,7 @@
 class Truck < Formula
   desc "Truck - the simplest dependency manager"
-  url "https://github.com/Mazyod/homebrew-truck/archive/0.1.4.zip"
-  version "0.1.4"
+  url "https://github.com/Mazyod/homebrew-truck/archive/0.1.5.zip"
+  version "0.1.5"
   # sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
 
   def install
